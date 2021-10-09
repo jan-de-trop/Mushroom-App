393cb68de201d10000e7ac199fac1dd4f95447bc
jan-de-trop <77740301+jan-de-trop@users.noreply.github.com>
2021-10-09 11:40:18 +0530

    Add files via upload

---
+++ database-server/db/migrations/20210913143004_leaderboard.sql
@@ -0,0 +1,22 @@
+-- migrate:up
+
+
+-- migrate:down
+
+CREATE TABLE leaderboard (
+    id BIGSERIAL PRIMARY KEY,
+    trainable_parameters NUMERIC,
+    execution_time NUMERIC,
+    loss NUMERIC,
+    accuracy NUMERIC,
+    model_size NUMERIC,
+    learning_rate NUMERIC,
+    batch_size NUMERIC,
+    epochs NUMERIC,
+    optimizer TEXT,
+    email TEXT,
+    experiment TEXT,
+    model_name TEXT
+);
+
+DROP TABLE IF EXISTS leaderboard;
